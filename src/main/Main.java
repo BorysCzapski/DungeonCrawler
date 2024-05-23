61953b1ed52f0d5322ce32f40d830d3307e7724f
BorysCzapski <113203258+BorysCzapski@users.noreply.github.com>
2024-05-23 09:32:38 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,21 +1,22 @@
-package main;
 
+import java.awt.Component;
 import javax.swing.JFrame;
 
 public class Main {
-		public static void main(String[] args) {
-			JFrame window = new JFrame();
-			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			window.setResizable(false);
-			window.setTitle("Dungeon Crawler 2D");
-			
-			GamePanel gamePanel = new GamePanel();
-			window.add(gamePanel);
-			
-			
-			window.pack();
-			
-			window.setLocationRelativeTo(null);
-			window.setVisible(true);
-		}
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(3);
+        window.setResizable(false);
+        window.setTitle("Dungeon Crawler 2D");
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+        window.setLocationRelativeTo((Component)null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
 }
